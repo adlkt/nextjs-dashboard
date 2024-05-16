95ead83f867dd1616a85fc1c74d9103519c0dc5c
adlkt <1625142032@qq.com>
2024-05-16 09:05:31 +0800

    two

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.devtool = 'eval-source-map'; // 或者 'cheap-module-source-map'
+    }
+    return config;
+  },
+
+  swcMinify: true, // 确保启用 SWC 压缩
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 module.exports = nextConfig;
